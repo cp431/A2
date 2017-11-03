5ef23eed15b3191e81d7521c76c5520436d59411
Gareth <garethjsharpe@gmail.com>
2017-11-03 13:19:58 -0400

    Update a2.c

--- a2.c
+++ a2.c
@@ -309,7 +309,10 @@ int main(int argc, char *argv[]) {
   MPI_Bcast(&array_data->subarray_a_indices, num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
   MPI_Bcast(&array_data->subarray_b_indices, num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
   printf("send\n");
+  printf("process: %d size of a: %d size of indices a: %d",process_rank,array_data->subarray_a_lengths[process_rank], array_data->subarray_a_indices[process_rank]); 
+  printf("process: %d size of b: %d size of indices b: %d",process_rank,array_data->subarray_b_lengths[process_rank], array_data->subarray_b_indices[process_rank]); 
   // Initialize start time
+  
   start_time = MPI_Wtime(); 
   
   // return the subarray lengths to each processor dependent on each processors rank
