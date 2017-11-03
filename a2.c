73565450d7b258245ef157871634afa0c4be9e2c
BradleyKatz <brad.katz13@gmail.com>
2017-11-03 14:44:40 -0400

    Update a2.c

--- a2.c
+++ a2.c
@@ -309,10 +309,10 @@ int main(int argc, char *argv[]) {
   }
   // prevent data broadcast and manipulation until array_data has been calculated
   MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Bcast(&array_data->subarray_a_lengths[0], num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
-  MPI_Bcast(&array_data->subarray_b_lengths[0], num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
-  MPI_Bcast(&array_data->subarray_a_indices[0], num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
-  MPI_Bcast(&array_data->subarray_b_indices[0], num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
+  MPI_Bcast(array_data->subarray_a_lengths, num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
+  MPI_Bcast(array_data->subarray_b_lengths, num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
+  MPI_Bcast(array_data->subarray_a_indices, num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
+  MPI_Bcast(array_data->subarray_b_indices, num_processors, MPI_INT, FIRST, MPI_COMM_WORLD);
   // Initialize start time
   
   start_time = MPI_Wtime(); 
